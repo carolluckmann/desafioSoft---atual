7062017d4d21c092317061ba8be811433b1d3b2f
carolluckmann <carolinaluckmann05@gmail.com>
2025-02-20 16:57:09 -0300

    iniciando javascript

---
+++ category.js
@@ -0,0 +1,10 @@
+const categoryName = document.getElementById("category");
+const tax = document.getElementById("tax");
+const table = document.getElementById("table");
+
+let categories = [];
+
+function searchCategory(){
+    
+}
+
